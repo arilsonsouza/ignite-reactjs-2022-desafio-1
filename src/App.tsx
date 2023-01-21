905d42cf6243d6b348c7795f07b64d2823e09997
Arilson Souza <arilsons61@gmail.com>
2023-01-21 15:49:06 -0300

    show task

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import { PlusCircle } from "phosphor-react";
+import { ClipboardText, PlusCircle } from "phosphor-react";
+import { useState } from "react";
 import styles from "./App.module.scss";
 import { Header } from "./components/Header";
+import { Task } from "./components/Task";
 
 function App() {
+  const [tasks, setTasks] = useState([1, 2, 3, 4, 5]);
+
   return (
     <div className={styles.app}>
       <Header />
@@ -14,6 +18,34 @@ function App() {
             <PlusCircle size={16} />
           </button>
         </form>
+
+        <div className={styles.content}>
+          <div className={styles.tasksInfo}>
+            <div className={styles.tasksCreated}>
+              <strong>Tarefas criadas</strong>
+              <span className={styles.tasksBadge}>5</span>
+            </div>
+            <div className={styles.tasksDone}>
+              <strong>Concluídas</strong>
+              <span className={styles.tasksBadge}>2 de 5</span>
+            </div>
+          </div>
+          <div className={styles.tasks}>
+            {tasks.length === 0 ? (
+              <div className={styles.tasksEmpty}>
+                <ClipboardText size="3.5rem" />
+                <strong>Você ainda não tem tarefas cadastradas</strong>
+                <span>Crie tarefas e organize seus itens a fazer</span>
+              </div>
+            ) : (
+              <>
+                {tasks.map((task) => (
+                  <Task />
+                ))}
+              </>
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
