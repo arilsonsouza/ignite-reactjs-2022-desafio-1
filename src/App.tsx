95cfc07f5eb8f2255086e33c9f5200fe44289932
Arilson Souza <arilsons61@gmail.com>
2023-01-21 11:37:31 -0300

    add create task form

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { PlusCircle } from "phosphor-react";
 import styles from "./App.module.scss";
+import { Header } from "./components/Header";
 
 function App() {
   return (
     <div className={styles.app}>
-      <h1>Todo App</h1>
+      <Header />
+      <div className={styles.container}>
+        <form>
+          <input type="text" placeholder="Adicione uma nova tarefa" />
+          <button type="submit">
+            Criar
+            <PlusCircle size={16} />
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
